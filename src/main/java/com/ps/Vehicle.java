e4db410956991ce5b13362b9da7da9e2fa4a74b4
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-05-11 08:10:37 -0400

    Vehicle part done

--- src/main/java/com/ps/Vehicle.java
+++ src/main/java/com/ps/Vehicle.java
@@ -1,4 +1,60 @@
 package com.ps;
 
 public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String type;
+    private String color;
+    private int odometer;
+    private double price;
+
+    public Vehicle(int vin, int year, String make, String model, String type, String color, int odometer, double price){
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.type = type;
+        this.color = color;
+        this.odometer = odometer;
+        this.price   = price;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return vin + "|" + year + "|" + make + "|" + model + "|" + type + "|" + color + "|" + odometer + "|" + price;
+    }
 }
